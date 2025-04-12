f1718be8dd1d0bbc6b46c15e31ecb058dfea26b2
Maximiliano Niscola <niscolamaximiliano@gmail.com>
2025-04-12 17:49:27 -0300

    tp_6_express

---
+++ src/server/server.js
@@ -0,0 +1,21 @@
+const express= require('express')
+const path=require('path')
+
+const startServer= (options) => {
+    const { port, public_path = 'public' } = options
+
+    const app = express()
+    
+    app.use(express.static(public_path))
+
+    app.get("/{*any}", (req, res) => {
+        const indexPath = path.join(__dirname + `../../../${public_path}`)
+        res.sendFile(indexPath)
+    })
+
+    app.listen(port, () => {
+        console.log(`Escuchando al puerto ${port}`)
+    })
+}
+
+module.exports={ startServer }
